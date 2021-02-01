b1417a424f1393c9b5b3eff4637608dbaad83a1f
BushraWaheed <72761774+bushrawaheed@users.noreply.github.com>
2021-02-01 12:41:17 +0100

    js3weekhw.js

--- shecodesplus.html/src/js arrays and loops/jshw.js
+++ shecodesplus.html/src/js arrays and loops/jshw.js
@@ -20,6 +20,20 @@ let weather = {
     humidity: 20
   }
 };
-function myFunction() {
-  alert("Enter a city name?");
+
+let city = prompt("what's your city?");
+city=city.toLowerCase();
+if(weather [city] !== undefined){
+    let temperature = weather[city].temp;
+    let humidity = weather[city].humidity;
+    let celsiusTemperature = Math.round(temperature);
+    let fahrenheitTemperature = Math.round((temperature *9) / 5 + 32);
+
+alert(
+    `It is currently ${celsiusTemperature}°C (${fahrenheitTemperature}°F) in ${city} with a humidity of ${humidity}%`
+    );
+}else{
+    alert(
+        `Sorry, we don't know the weather for this city, try going to https://www.google.com/search?q=weather+${city}`
+    );
 }
